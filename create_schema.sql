0b7f40c0f3382fb926f36d020471a3d68f5ad28e
Halsey Filbin <halsey.t.filbin@gmail.com>
2023-10-28 15:00:39 -0500

    SQL-Basics

--- create_schema.sql
+++ create_schema.sql
@@ -7,3 +7,30 @@ CREATE TABLE students (
   birthdate    date NOT NULL,
   address_id   integer
 );
+
+DROP TABLE IF EXISTS addresses;
+CREATE TABLE addresses (
+  id          serial PRIMARY KEY,
+  line_1      VARCHAR(50),
+  city        VARCHAR(30),
+  state       VARCHAR(20),
+  zipcode     CHAR(5)
+);
+
+DROP TABLE IF Exists classes;
+CREATE TABLE classes (
+  id          serial PRIMARY KEY,
+  name        VARCHAR(30),
+  credits     integer
+);
+
+DROP TABLE IF EXISTS enrollments;
+CREATE TABLE enrollments (
+  id          serial PRIMARY KEY,
+  student_id  integer,
+  class_id    integer,
+  grade       CHAR(1)
+);
+
+
+
